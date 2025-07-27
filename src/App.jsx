e7c1a7548a1048368d86923bedda4b9d24814c64
Yeassin7376 <yeassinali17@gmail.com>
2025-07-27 07:12:57 +0600

    Project creation, package installation and first commit done

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-5xl text-gray-500'>Newspaper - assignment12_category_010</h1>
+    </>
+  )
+}
+
+export default App
